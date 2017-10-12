8bb57b846b7d76e850949f679d713731f207a2c0
Martin Sidaway <msidaway@gmail.com>
2017-10-12 17:00:50 +0100

    Moved smt2 generator into ntshared.py; send extraneous output to stderr.

--- ntshared.py
+++ ntshared.py
@@ -18,6 +18,9 @@ import matplotlib.pyplot as plt
 
 BATCH_SIZE = 100
 
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
 def random_data(size=BATCH_SIZE):
     pq = R.rand(size, 2) * 2.0 - 1.0
     pqt = pq.transpose()
@@ -65,8 +68,8 @@ def setup_network():
     t = nn.Variable(label.shape)
     y, hs = mlp(x, [8])
 
-    print (y.shape)
-    print (t.shape)
+    eprint (y.shape)
+    eprint (t.shape)
 
     loss = F.mean(F.softmax_cross_entropy(y, t))
 
@@ -84,13 +87,13 @@ def train_network(loss):
         solver.weight_decay(1e-5)
         solver.update()
         if i % 100 == 0:
-            print(i, loss.d)
+            eprint(i, loss.d)
 
     return solver
 
 def predict(pq, label):
     x.d, t.d = pq, label
-    print(t.d.reshape(BATCH_SIZE))
+    eprint(t.d.reshape(BATCH_SIZE))
     loss.forward()
     preds = y.d.argmax(axis=1)
     return preds, loss
@@ -101,5 +104,79 @@ def seed():
     else:
         seed = np.int64(np.float64(time.time()).view(np.uint64) % 2**32)
 
-    print("Seed:", seed)
+    eprint("Seed:", seed)
     R.seed(seed)
+
+def nnabla_to_smt2_info(var, names={}, collect={}, rcollect={}, vars=[],
+                        assertions=[], nid=0, normal=True):
+
+    if var in rcollect:
+        return collect  # already processed this variable
+    rcollect[var] = nid
+    if var not in names:
+        names[var] = 'var_{}'.format(nid)
+    collect[nid] = var
+    cur_name = names[var]
+    if normal:
+        assert len(var.shape) == 2
+        for index in range(var.shape[1]):
+            vars.append('{}_{}'.format(cur_name, index))
+    nid += 1
+    if var.parent is not None:
+        eprint(var.parent)
+        eprint(var.parent.inputs)
+        eprint(type(var.parent.inputs))
+        for index, input in enumerate(var.parent.inputs):
+            _, _, _, nid = nnabla_to_smt2_info(input, names, collect, rcollect,
+                                               vars, assertions, nid, index == 0)
+
+        if var.parent.name == 'ReLU':
+            assert normal
+            assert len(var.parent.inputs) == 1
+            assert var.parent.inputs[0].shape == var.shape
+            param_name = names[var.parent.inputs[0]]
+            for index in range(var.shape[1]):
+                assertions.append('(= {}_{} (max 0 {}_{}))'.format(
+                    cur_name, index, param_name, index
+                ))
+        elif var.parent.name == 'Affine':
+            # Wx + b -- W and b are trained parameters
+            assert normal
+            assert len(var.parent.inputs) == 3
+            var_x = var.parent.inputs[0]
+            var_W = var.parent.inputs[1]
+            var_b = var.parent.inputs[2]
+            assert len(var_x.shape) == 2
+            assert len(var_W.shape) == 2
+            assert len(var_b.shape) == 1
+            assert var_W.shape[0] == var_x.shape[1]
+            assert var_W.shape[1] == var.shape[1]
+            assert var_W.shape[1] == var_b.shape[0]
+            x_name = names[var_x]
+            for i in range(var.shape[1]):
+                terms = []
+                for j in range(var_x.shape[1]):
+                    terms.append('(* {} {}_{})'.format(
+                        var_W.d[j][i],
+                        x_name,
+                        j
+                    ))
+                assertions.append('(= {}_{} (+ {} {}))'.format(
+                    cur_name,
+                    i,
+                    var_b.d[i],
+                    ' '.join(terms)
+                ))
+        else:
+            raise Exception('Unsupported function: {}'.format(var.parent.name))
+    return collect, vars, assertions, nid
+
+def nnabla_to_smt2(var, names={}):
+    collect, vars, assertions, _ = nnabla_to_smt2_info(var, names)
+    smt2 = ''
+    smt2 += '(set-logic QF_NRA)\n'
+    smt2 += ''.join(map(lambda n: '(declare-fun {} () Real)\n'.format(n), vars))
+    smt2 += ''.join(map(lambda a: '(assert {})\n'.format(a), assertions))
+    smt2 += '(check-sat)\n'
+    smt2 += '(exit)\n'
+    return smt2
